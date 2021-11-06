92616e5fe75abdf3d913bc3834d98628592a9055
Sloan <Sloan.boyce@gmail.com>
2021-11-05 23:09:14 -0400

    added buttons for each hour, currently not functional

--- Develop/script.js
+++ Develop/script.js
@@ -9,42 +9,50 @@ function updateTime() {
     checkPastFuture();
 }
 
-
-// add buttons for each time block 
-// commit this tomorrow 
 function initWorkTime() {
     for(let i = 9; i < 18; i++) {
          timeContainersEl[i] = document.createElement("div");
-         timeContainersEl[i].classList = "time-block row text-light " + i.toString();
+         timeContainersEl[i].classList = "time-block row hour " + i.toString();
          workHours.append(timeContainersEl[i]);
          timeFormEl[i] = document.createElement("form");
          $(timeFormEl[i]).addClass("text-light")
          let inputArea = document.createElement("textarea");
-         inputArea.className = "description"
+         $(inputArea).addClass("description");
+         let saveBtn = document.createElement("button");
+         $(saveBtn).addClass("saveBtn");
+         $(saveBtn).text("save");
 
          if(i < 10) {
             timeContainersEl[i].innerHTML = "<h2>0" + i + ":00 </h2>"
          }else {
              timeContainersEl[i].innerHTML = "<h2>" + i + ":00 </h2>"
          }
+         
          $(timeFormEl[i]).append(inputArea);
+         $(timeFormEl[i]).append(saveBtn);
          $(timeContainersEl[i]).append(timeFormEl[i]);
     }
+
 }
 
 function checkPastFuture() {
     for(let i = 0; i < timeContainersEl.length; i++) {
         if(i > moment().hour()) {
-            $(timeContainersEl[i]).addClass("bg-success");
+            $(timeContainersEl[i]).addClass("future");
             console.log("future")
         }else if(i == moment().hour()) {
-            $(timeContainersEl[i]).addClass("bg-danger");
+            $(timeContainersEl[i]).addClass("present");
             console.log("present");
         }else if(i < moment().hour()) {
-            $(timeContainersEl[i]).addClass("bg-secondary");
+            $(timeContainersEl[i]).addClass("past");
         }
     }
 }
+
+// get this working tomorrow 
+$(".saveBtn").submit((event) => {
+    console.log("Hello");
+});
 
 initWorkTime();
 checkPastFuture();
